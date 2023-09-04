60c9a37dfbbc780f236ae14e4140159f90361c4d
Ashiqur Rahman <imashiqe@gmail.com>
2023-09-04 15:09:17 +0600

    project setup (#2)
    * server and database configuration complete
    
    * [done] cors , express , mongodb , parser

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import config from './config/index'
+import app from './app'
+async function boostrap() {
+    try{
+        await mongoose.connect(config.database_url as string);
+        console.log(`Datqabase is connected Successfully`)
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+          });
+
+
+    }catch(err){
+      console.log(`Failed to connect database` , err)
+    }
+   
+  
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
+
+  boostrap();
