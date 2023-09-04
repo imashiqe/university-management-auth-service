1a062d61eed634b3848275702a4885f8fe8aae6a
Ashiqur Rahman <imashiqe@gmail.com>
2023-09-04 15:05:17 +0600

    [done] cors , express , mongodb , parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express, { Application } from 'express'
+import express, { Application, Request,Response } from 'express';
+import cors from 'cors';
 const app : Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Testing
+app.get('/', (req: Request, res:Response) => {
+  res.send('Working Successfully University Management');
+});
 
 
 export default app
